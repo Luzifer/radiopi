57d45208ffcea9c8640647d2fcd1adc6daf2f934
Knut Ahlers <knut@ahlers.me>
2015-08-07 11:45:16 +0200

    Check for updates every 10min

--- autoupdate.go
+++ autoupdate.go
@@ -1,10 +1,15 @@
 package main
 
-import "github.com/Luzifer/gobuilder/autoupdate"
+import (
+	"time"
+
+	"github.com/Luzifer/gobuilder/autoupdate"
+)
 
 func init() {
 	updater := autoupdate.New("github.com/Luzifer/radiopi", "master")
 	updater.SelfRestart = true
+	updater.UpdateInterval = 10 * time.Minute
 	if version != "dev" {
 		go updater.Run()
 	}
